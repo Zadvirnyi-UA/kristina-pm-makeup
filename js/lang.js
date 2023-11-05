1197f202be31ba17e54ac2de0c6bd095985be629
Zadvirnyi-UA <110035502+Zadvirnyi-UA@users.noreply.github.com>
2023-11-05 16:24:37 +0100

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,192 @@
+const langArr = {
+    "menu__link-about" : {
+        "ua" : "Про мене",
+        "pl" : "O mnie",
+        "en" : "About me",
+    },
+    "menu__link-procedure" : {
+        "ua" : "Про процедуру",
+        "pl" : "Procedura",
+        "en" : "Procedure",
+    },
+    "menu__link-services" : {
+        "ua" : "Послуги",
+        "pl" : "Usługi",
+        "en" : "Services",
+    },
+    "menu__link-contact" : {
+        "ua" : "Контакт",
+        "pl" : "Kontakt",
+        "en" : "Contact",
+    },
+    "about__title" : {
+        "ua" : "Крістіна Власенко",
+        "pl" : "Kristina Vlasenko",
+        "en" : "Kristina Vlasenko",
+    },
+    "about__subtitle" : {
+        "ua" : "Майстер перманентного макіяжу",
+        "pl" : "Mistrz makijażu permanentnego",
+        "en" : "Master of permanent makeup",
+    },
+    "about__quote" : {
+        "ua" : "Ранок починається з кави а не з макіяжу!",
+        "pl" : "Ranek zaczyna się od kawy, a nie od makijażu!",
+        "en" : "The morning starts with coffee, not makeup!",
+    },
+    "procedure__title" : {
+        "ua" : "Що таке перманентний макіяж?",
+        "pl" : "Czym jest makijaż permanentny?",
+        "en" : "What is permanent makeup?",
+    },
+    "procedure__text-desc" : {
+        "ua" : "Перманентний макіяж - це процедура, при якій спеціаліст вводить пігмент під шкіру, щоб створити ефект макіяжу, який залишається стійким на протязі тривалого періоду часу.",
+        "pl" : "Makijaż permanentny to procedura, w której specjalista wprowadza pigment pod skórę, aby stworzyć efekt makijażu, który pozostaje stabilny przez długi czas.",
+        "en" : "Permanent makeup is a procedure in which a specialist inserts pigment under the skin to create a makeup effect that remains stable for a long time.",
+    },
+    "procedure__title-advantages" : {
+        "ua" : "Основні переваги:",
+        "pl" : "Główne plusy:",
+        "en" : "Main advantages:",
+    },
+    "time-saving" : {
+        "ua" : "Збереження часу: дозволяє заощадити час на щоденному нанесенні макіяжу, оскільки він залишається стійким протягом місяців або навіть років.",
+        "pl" : "Oszczędność czasu: pozwala zaoszczędzić czas podczas codziennej stylizacji makijażu, ponieważ utrzymuje się przez wiele miesięcy, a nawet lat.",
+        "en" : "Saves time: saves time on daily makeup styling as it lasts for months or even years.",
+    },
+    "improvement" : {
+        "ua" : "Покращення вигляду: вирівнює та підкреслює контури обличчя, очей, губ і брів, надаючи природний або бажаний вигляд.",
+        "pl" : "Ulepsz wygląd: Wygładza i podkreśla kontury twarzy, oczu, ust i brwi, zapewniając naturalny lub pożądany wygląd.",
+        "en" : "Enhance the look: Smooths and enhances the contours of the face, eyes, lips and eyebrows for a natural or desired look.",
+    },
+    "correction" : {
+        "ua" : "Корекція дефектів: використовується для корекції дефектів шкіри, таких як рубці або пігментація.",
+        "pl" : "Korekta defektów: stosowana do korygowania niedoskonałości skóry, takich jak blizny lub pigmentacja.",
+        "en" : "Defect correction: used to correct skin imperfections such as scars or pigmentation.",
+    },
+    "stability" : {
+        "ua" : "Стійкість: залишається стійким під час спорту, водних процедур і в різних погодних умовах.",
+        "pl" : "Trwałość: zachowuje stabilność podczas uprawiania sportu, zabiegów wodnych i w różnych warunkach pogodowych.",
+        "en" : "Durability: remains stable during sports, water treatments and in various weather conditions.",
+    },
+    "procedure__title-relative-contraindication" : {
+        "ua" : "Відносне протипоказання до процедури:",
+        "pl" : "Względne przeciwwskazania do zabiegu:",
+        "en" : "Relative contraindications to the procedure:",
+    },
+    "pregnancy" : {
+        "ua" : "Вагітність",
+        "pl" : "Ciąża",
+        "en" : "Pregnancy",
+    },
+    "lactation" : {
+        "ua" : "Період лактації",
+        "pl" : "Okres karmienia piersią",
+        "en" : "Breastfeeding period",
+    },
+    "moles" : {
+        "ua" : "Родимки, бородавки, папіломи в зоні татуажу",
+        "pl" : "Znamiona, brodawki, brodawczaki w obszarze tatuażu",
+        "en" : "Nevi, warts, papillomas in the tattoo area",
+    },
+    "dermatitis" : {
+        "ua" : "Дерматити",
+        "pl" : "Zapalenie skóry",
+        "en" : "Dermatitis",
+    },
+    "neurodermatitis" : {
+        "ua" : "Нейродерміти",
+        "pl" : "Neurodermit",
+        "en" : "Neurodermit",
+    },
+    "allergy" : {
+        "ua" : "Аллергія на анестезію",
+        "pl" : "Alergia na znieczulenie",
+        "en" : "Allergy to anesthesia",
+    },
+    "herpes" : {
+        "ua" : "Герпес, заїди",
+        "pl" : "Opryszczka",
+        "en" : "Herpes",
+    },
+    "menstruation" : {
+        "ua" : "Період менструального циклу",
+        "pl" : "Okres cyklu menstruacyjnego",
+        "en" : "Period of the menstrual cycle",
+    },
+    "procedure__title-absolut-contraindication" : {
+        "ua" : "Абсолютні протипоказання до процедури:",
+        "pl" : "Bezwzględne przeciwwskazania do zabiegu:",
+        "en" : "Absolute contraindications to the procedure:",
+    },
+    "diabetes" : {
+        "ua" : "Інсуліно залежна форма цукрового діабету",
+        "pl" : "Insulinozależna postać cukrzycy",
+        "en" : "Insulin-dependent form of diabetes",
+    },
+    "blood" : {
+        "ua" : "Хвороби пов'язані зі значним зниженням згортання крові",
+        "pl" : "Choroby związane ze znacznym zmniejszeniem krzepliwości krwi",
+        "en" : "Diseases associated with a significant reduction in blood clotting",
+    },
+    "inflammation" : {
+        "ua" : "Гострі запальні захворювання",
+        "pl" : "Ostre choroby zapalne",
+        "en" : "Acute inflammatory diseases",
+    },
+    "disorders" : {
+        "ua" : "Психічні розлади",
+        "pl" : "Zaburzenia psychiczne",
+        "en" : "Mental disorders",
+    },
+    "epilepsy" : {
+        "ua" : "Епілепсія",
+        "pl" : "Padaczka",
+        "en" : "Epilepsy",
+    },
+    "viruses" : {
+        "ua" : "Гепатит, ВІЛ, СНІД",
+        "pl" : "Wirusowe zapalenie wątroby, HIV, AIDS",
+        "en" : "Viral hepatitis, HIV, AIDS",
+    },
+    "scars" : {
+        "ua" : "Схильності до келоїдних рубців",
+        "pl" : "Predyspozycje do powstawania blizn keloidowych",
+        "en" : "Predisposition to the formation of keloid scars",
+    },
+    "services" : {
+        "ua" : "Які послуги я надаю",
+        "pl" : "Jakie usługi świadczę",
+        "en" : "What services I provide",
+    },
+    "services-eyebrows" : {
+        "ua" : "Брови первична / корекція",
+        "pl" : "Brwi pierwotna / korekta",
+        "en" : "Eyebrows primary / correction",
+    },
+    "services-lips" : {
+        "ua" : "Губи первична / корекція",
+        "pl" : "Usta pierwotna / korekta",
+        "en" : "Lips primary / correction",
+    },
+    "works" : {
+        "ua" : "Мої роботи",
+        "pl" : "Moje prace",
+        "en" : "My works",
+    },
+    // "reviews" : {
+    //     "ua" : "Відгуки",
+    //     "pl" : "Recenzje",
+    //     "en" : "Reviews",
+    // },
+    "footer__title" : {
+        "ua" : "Перманентний макіяж",
+        "pl" : "Makijaż permanentny",
+        "en" : "Permanent makeup",
+    },
+    "rights" : {
+        "ua" : "всі права захищенно,",
+        "pl" : "wszystkie prawa zastrzeżone,",
+        "en" : "all rights reserved,",
+    },
+} 
