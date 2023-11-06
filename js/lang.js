084cbd72af42e4aa3cadd757186d20368be99234
Zadvirnyi-UA <110035502+Zadvirnyi-UA@users.noreply.github.com>
2023-11-06 21:26:14 +0100

    v 1,1

--- js/lang.js
+++ js/lang.js
@@ -30,7 +30,7 @@ const langArr = {
         "en" : "Master of permanent makeup",
     },
     "about__quote" : {
-        "ua" : "Ранок починається з кави а не з макіяжу!",
+        "ua" : "Ранок починається з кави, а не з макіяжу!",
         "pl" : "Ranek zaczyna się od kawy, a nie od makijażu!",
         "en" : "The morning starts with coffee, not makeup!",
     },
@@ -40,7 +40,7 @@ const langArr = {
         "en" : "What is permanent makeup?",
     },
     "procedure__text-desc" : {
-        "ua" : "Перманентний макіяж - це процедура, при якій спеціаліст вводить пігмент під шкіру, щоб створити ефект макіяжу, який залишається стійким на протязі тривалого періоду часу.",
+        "ua" : "Перманентний макіяж - це процедура, при якій спеціаліст вводить пігмент під шкіру, щоб створити ефект макіяжу, який залишається стійким упродовж тривалого періоду часу.",
         "pl" : "Makijaż permanentny to procedura, w której specjalista wprowadza pigment pod skórę, aby stworzyć efekt makijażu, który pozostaje stabilny przez długi czas.",
         "en" : "Permanent makeup is a procedure in which a specialist inserts pigment under the skin to create a makeup effect that remains stable for a long time.",
     },
@@ -49,110 +49,30 @@ const langArr = {
         "pl" : "Główne plusy:",
         "en" : "Main advantages:",
     },
-    "time-saving" : {
-        "ua" : "Збереження часу: дозволяє заощадити час на щоденному нанесенні макіяжу, оскільки він залишається стійким протягом місяців або навіть років.",
-        "pl" : "Oszczędność czasu: pozwala zaoszczędzić czas podczas codziennej stylizacji makijażu, ponieważ utrzymuje się przez wiele miesięcy, a nawet lat.",
-        "en" : "Saves time: saves time on daily makeup styling as it lasts for months or even years.",
-    },
-    "improvement" : {
-        "ua" : "Покращення вигляду: вирівнює та підкреслює контури обличчя, очей, губ і брів, надаючи природний або бажаний вигляд.",
-        "pl" : "Ulepsz wygląd: Wygładza i podkreśla kontury twarzy, oczu, ust i brwi, zapewniając naturalny lub pożądany wygląd.",
-        "en" : "Enhance the look: Smooths and enhances the contours of the face, eyes, lips and eyebrows for a natural or desired look.",
-    },
-    "correction" : {
-        "ua" : "Корекція дефектів: використовується для корекції дефектів шкіри, таких як рубці або пігментація.",
-        "pl" : "Korekta defektów: stosowana do korygowania niedoskonałości skóry, takich jak blizny lub pigmentacja.",
-        "en" : "Defect correction: used to correct skin imperfections such as scars or pigmentation.",
-    },
-    "stability" : {
-        "ua" : "Стійкість: залишається стійким під час спорту, водних процедур і в різних погодних умовах.",
-        "pl" : "Trwałość: zachowuje stabilność podczas uprawiania sportu, zabiegów wodnych i w różnych warunkach pogodowych.",
-        "en" : "Durability: remains stable during sports, water treatments and in various weather conditions.",
+    "procedure__desc-advantages" : {
+        "ua" : "Дозволяє заощадити час на щоденному нанесенні макіяжу, оскільки він залишається стійким протягом місяців або навіть років. Вирівнює та підкреслює контури обличчя, очей, губ і брів, надаючи природний або бажаний вигляд. Використовується для корекції дефектів шкіри, таких як рубці або пігментація. Залишається стійким під час спорту, водних процедур і в різних погодних умовах.",
+        "pl" : "Oszczędza czas podczas codziennej aplikacji makijażu, ponieważ utrzymuje się przez wiele miesięcy, a nawet lat. Wygładza i podkreśla kontury twarzy, oczu, ust i brwi, zapewniając naturalny lub pożądany wygląd. Służy do korygowania niedoskonałości skóry, takich jak blizny lub przebarwienia. Pozostaje stabilny podczas uprawiania sportu, zabiegów wodnych i w różnych warunkach pogodowych.",
+        "en" : "It saves time in daily makeup application as it lasts for months or even years. Smooths and enhances the contours of the face, eyes, lips and eyebrows for a natural or desired look. It is used to correct skin imperfections, such as scars or blemishes. Remains stable during sports, water treatments and in various weather conditions.",
     },
     "procedure__title-relative-contraindication" : {
         "ua" : "Відносне протипоказання до процедури:",
         "pl" : "Względne przeciwwskazania do zabiegu:",
         "en" : "Relative contraindications to the procedure:",
     },
-    "pregnancy" : {
-        "ua" : "Вагітність",
-        "pl" : "Ciąża",
-        "en" : "Pregnancy",
-    },
-    "lactation" : {
-        "ua" : "Період лактації",
-        "pl" : "Okres karmienia piersią",
-        "en" : "Breastfeeding period",
-    },
-    "moles" : {
-        "ua" : "Родимки, бородавки, папіломи в зоні татуажу",
-        "pl" : "Znamiona, brodawki, brodawczaki w obszarze tatuażu",
-        "en" : "Nevi, warts, papillomas in the tattoo area",
-    },
-    "dermatitis" : {
-        "ua" : "Дерматити",
-        "pl" : "Zapalenie skóry",
-        "en" : "Dermatitis",
-    },
-    "neurodermatitis" : {
-        "ua" : "Нейродерміти",
-        "pl" : "Neurodermit",
-        "en" : "Neurodermit",
-    },
-    "allergy" : {
-        "ua" : "Аллергія на анестезію",
-        "pl" : "Alergia na znieczulenie",
-        "en" : "Allergy to anesthesia",
-    },
-    "herpes" : {
-        "ua" : "Герпес, заїди",
-        "pl" : "Opryszczka",
-        "en" : "Herpes",
-    },
-    "menstruation" : {
-        "ua" : "Період менструального циклу",
-        "pl" : "Okres cyklu menstruacyjnego",
-        "en" : "Period of the menstrual cycle",
+    "procedure__desc-relative-contraindication" : {
+        "ua" : "Вагітність. Період лактації. Родимки, бородавки, папіломи в зоні татуажу. Дерматити. Нейродерміти. Аллергія на анестезію. Герпес, заїди. Період менструального циклу.",
+        "pl" : "Ciąża. Okres laktacji. Znamiona, brodawki, brodawczaki w obszarze tatuażu. Zapalenie skóry. Neurodermit. Alergia na znieczulenie. Opryszczka, świerzb. Okres cyklu miesiączkowego.",
+        "en" : "Pregnancy. Lactation period. Nevi, warts, papillomas in the tattoo area. Dermatitis. Neurodermatitis. Allergy to anesthesia. Herpes, scabies. Period of the menstrual cycle.",
     },
     "procedure__title-absolut-contraindication" : {
         "ua" : "Абсолютні протипоказання до процедури:",
         "pl" : "Bezwzględne przeciwwskazania do zabiegu:",
         "en" : "Absolute contraindications to the procedure:",
     },
-    "diabetes" : {
-        "ua" : "Інсуліно залежна форма цукрового діабету",
-        "pl" : "Insulinozależna postać cukrzycy",
-        "en" : "Insulin-dependent form of diabetes",
-    },
-    "blood" : {
-        "ua" : "Хвороби пов'язані зі значним зниженням згортання крові",
-        "pl" : "Choroby związane ze znacznym zmniejszeniem krzepliwości krwi",
-        "en" : "Diseases associated with a significant reduction in blood clotting",
-    },
-    "inflammation" : {
-        "ua" : "Гострі запальні захворювання",
-        "pl" : "Ostre choroby zapalne",
-        "en" : "Acute inflammatory diseases",
-    },
-    "disorders" : {
-        "ua" : "Психічні розлади",
-        "pl" : "Zaburzenia psychiczne",
-        "en" : "Mental disorders",
-    },
-    "epilepsy" : {
-        "ua" : "Епілепсія",
-        "pl" : "Padaczka",
-        "en" : "Epilepsy",
-    },
-    "viruses" : {
-        "ua" : "Гепатит, ВІЛ, СНІД",
-        "pl" : "Wirusowe zapalenie wątroby, HIV, AIDS",
-        "en" : "Viral hepatitis, HIV, AIDS",
-    },
-    "scars" : {
-        "ua" : "Схильності до келоїдних рубців",
-        "pl" : "Predyspozycje do powstawania blizn keloidowych",
-        "en" : "Predisposition to the formation of keloid scars",
+    "procedure__desc-absolut-contraindication" : {
+        "ua" : "Інсуліно залежна форма цукрового діабету. Хвороби пов'язані зі значним зниженням згортання крові. Гострі запальні захворювання. Психічні розлади. Епілепсія. Гепатит, ВІЛ, СНІД. Схильності до келоїдних рубців.",
+        "pl" : "Insulinozależna postać cukrzycy. Choroby związane ze znacznym zmniejszeniem krzepliwości krwi. Ostre choroby zapalne. Zaburzenia psychiczne. Padaczka. Zapalenie wątroby, HIV, AIDS. Predyspozycje do powstawania blizn keloidowych.",
+        "en" : "Insulin-dependent form of diabetes. Diseases associated with a significant reduction in blood clotting. Acute inflammatory diseases. Mental disorders. Epilepsy. Hepatitis, HIV, AIDS. Predisposition to the formation of keloid scars.",
     },
     "services" : {
         "ua" : "Які послуги я надаю",
@@ -174,11 +94,6 @@ const langArr = {
         "pl" : "Moje prace",
         "en" : "My works",
     },
-    // "reviews" : {
-    //     "ua" : "Відгуки",
-    //     "pl" : "Recenzje",
-    //     "en" : "Reviews",
-    // },
     "footer__title" : {
         "ua" : "Перманентний макіяж",
         "pl" : "Makijaż permanentny",
